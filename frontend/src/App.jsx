806600c55c5f4d64b9b13fec552f6b36b1870552
DeveloperWizard83 <laaksomik@gmail.com>
2024-09-25 16:47:59 +0300

    Fix frontend directory structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import AppointmentScheduler from './AppointmentScheduler'
+
+function App() {
+  
+
+  return (
+    <>
+     <AppointmentScheduler />
+    </>
+  )
+}
+
+export default App
